7c66918ac577ec21a0374c3f396cda813c0ea843
Hammad153 <hammadismail1546@gmail.com>
2025-09-11 12:43:38 +0100

    fix layout issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Space_Grotesk } from "next/font/google"
-import "./globals.css"
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import type React from "react";
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "@/context/AuthContext";
 
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] })
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LUXELA",
   description: "E-commerce platform for authentic fashion",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth max-w-[1440px] overflow-x-hidden mx-auto">
-      <body className={spaceGrotesk.className}>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
+    <html
+      lang="en"
+      className="scroll-smooth max-w-[1440px] overflow-x-hidden mx-auto">
+      <body suppressHydrationWarning className={spaceGrotesk.className}>
+        <AuthProvider>{children}</AuthProvider>
         <ToastContainer />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
